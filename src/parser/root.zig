7628082beb253404a2d3096fc522351b95eea0b9
Ruinedmango <00rudystx@gmail.com>
2026-01-14 23:36:26 -0900

    work on parser

--- src/parser/root.zig
+++ src/parser/root.zig
@@ -1,4 +1,15 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
-const lexer = @import("ignis_lexer");
+const lex = @import("ignis_lexer");
+
+pub const ExprKind = enum { Number, Ident, Unary, Binary, Call };
+pub const Expr = union(enum) { Number: struct { value: f64 }, Ident: struct { slice: []u8 }, Unary: struct { op: lex.TType, rhs } };
+
+pub const StmtKind = enum {};
+const Stmt = union(enum) {};
+
+const Parser = struct {
+    lexer: lex.Lexer,
+    alloc: Allocator,
+};
